fb82891e969b786261a15225f09c4cbdced604d8
Parviz Khorrami <pkhorraminejad@ymail.com>
2015-12-20 21:24:33 +0330

    Slight Changes

--- pilot_manager.sql
+++ pilot_manager.sql
@@ -3,11 +3,23 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Nov 24, 2015 at 11:02 AM
+-- Generation Time: Dec 20, 2015 at 09:21 PM
 -- Server version: 5.5.45-cll-lve
 -- PHP Version: 5.4.31
 
---Table structure for table `pilot_manager`
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pilot_manager`
 --
 
 CREATE TABLE IF NOT EXISTS `pilot_manager` (
