4a0ac24a1239b2f5f95312d2d48628c70a4aae0d
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-12-08 11:51:05 +0330

    Slight Change

--- Admin/Templates/pm/email_welcome.php
+++ Admin/Templates/pm/email_welcome.php
@@ -1,5 +1,5 @@
 <p>Dear <?php echo $pilot->firstname.' '.$pilot->lastname ;?>,</p>
-<p>Welcome to <?php echo SITE_NAME ;?>. You are now requiered to satrt your first flight. Please visit us at</p>
+<p>Welcome to <?php echo SITE_NAME ;?>. You are now required to satrt your first flight. Please visit us at</p>
 <p><?php echo SITE_URL ;?> to get started.</p>
 <p>Sincerely</p>
 <p><?php echo SITE_NAME ;?> - Staff</p>
